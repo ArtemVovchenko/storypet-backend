8c03129f4cf8c5153c20f4b677ab512061f9c7a9
Artem Vovchenko <artem.vovchenko@nure.ua>
2021-04-26 02:22:14 +0300

    Added middleware for passing user roles via request context

--- internal/app/middleware/access_permissions.go
+++ internal/app/middleware/access_permissions.go
@@ -4,14 +4,21 @@ import (
 	"errors"
 	"github.com/ArtemVovchenko/storypet-backend/internal/app/permissions"
 	"github.com/ArtemVovchenko/storypet-backend/internal/pkg/auth"
+	"golang.org/x/net/context"
 	"net/http"
 )
 
-var errAccessDenied = errors.New("access denied")
+type CtxKeys int
 
 type AccessPermissionMiddleware struct {
 	server server
 }
+
+const (
+	CtxUserRoles = iota
+)
+
+var errAccessDenied = errors.New("access denied")
 
 func NewAccessPermissionMiddleware(server server) *AccessPermissionMiddleware {
 	return &AccessPermissionMiddleware{server: server}
@@ -54,5 +61,22 @@ func (m *AccessPermissionMiddleware) DatabaseAccess(next http.HandlerFunc) http.
 			return
 		}
 		next(w, r)
+	}
+}
+
+func (m *AccessPermissionMiddleware) AddRolesToContext(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		accessMeta, err := auth.ExtractAccessMeta(r)
+		if err != nil {
+			m.server.RespondError(w, r, http.StatusForbidden, errAccessDenied)
+			return
+		}
+		session, err := m.server.PersistentStore().GetSessionInfo(accessMeta.AccessUUID)
+		if err != nil {
+			m.server.RespondError(w, r, http.StatusForbidden, errAccessDenied)
+			return
+		}
+
+		next(w, r.WithContext(context.WithValue(r.Context(), CtxUserRoles, session.Roles)))
 	}
 }
