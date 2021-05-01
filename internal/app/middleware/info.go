3156e853a01c3517f26981126c2d7dc9dcbbb629
Artem Vovchenko <44243084+ArtemVovchenko@users.noreply.github.com>
2021-05-01 23:35:42 +0300

    OPTIONS requests added to all api

--- internal/app/middleware/info.go
+++ internal/app/middleware/info.go
@@ -35,12 +35,11 @@ func (m *InfoMiddleware) MarkRequest(next http.Handler) http.Handler {
 
 func (m *InfoMiddleware) LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := &responseWriter{w, http.StatusOK}
 		if r.Method == http.MethodOptions {
 			return
 		}
 		m.server.Logger().Printf("Request %s %s : ID: %s", r.Method, r.RequestURI, r.Header.Get(hdrReqUUID))
-		rw := &responseWriter{w, http.StatusOK}
-
 		start := time.Now()
 		next.ServeHTTP(rw, r)
 		m.server.Logger().Printf("Request %s status=%v %v completed in %v",
